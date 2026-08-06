f5f528ac2483d28899f558af966ae67714e7a131
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-06 19:04:18 +0500

    Bootstrap reviewed File 14 completion source

--- global-clinic-usp/templates/footer-mission.php
+++
@@ -1,5 +0,0 @@
-<?php defined( 'ABSPATH' ) || exit; ?>
-<div class="sgc-footer-mission" role="complementary" aria-label="<?php echo esc_attr__( 'Global Clinic mission', 'global-clinic-usp' ); ?>">
-	<span><?php esc_html_e( 'Independent doctors. Responsible global access. Transparent platform governance.', 'global-clinic-usp' ); ?></span>
-	<a href="<?php echo esc_url( $mission_url ); ?>"><?php esc_html_e( 'Read Our Mission', 'global-clinic-usp' ); ?></a>
-</div>
